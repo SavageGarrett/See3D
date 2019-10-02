ac29ae0ceb3a79e00137e4b2c9da5ed14f32d5ff
Garrett <garrettcarder@gmail.com>
2019-10-02 16:04:10 -0400

    Publish Script

---
+++ pub.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Moves files from GitHub clone to web directory
+# Move to public_html directory before running
+
+mv See3D/css/* ./css
+mv See3D/img_refactor/* ./img_refactor
+mv See3D/js/* ./js
+mv See3D/original\ images/* ./original\ images
+mv See3D/testimonies/* ./testimonies/*
+mv See3D/*
